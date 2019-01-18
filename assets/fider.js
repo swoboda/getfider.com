8f0c1e3063cb410e088ac351b3d08a10ea789993
goenning <me@goenning.net>
2019-01-18 18:36:14 +0000

    multi currency

--- assets/fider.js
+++ assets/fider.js
@@ -1,4 +1,22 @@
+var euCountries = ["AT","BE","BG","CY","CZ","DE","DK","EE","EL","ES","FI","FR","GB","HR","HU","IE","IT","LT","LU","LV","MT","NL","PL","PT","RO","SE","SI","SK"];
+
+function setCurrency(symbol) {
+  if (sessionStorage) {
+    sessionStorage.setItem("currency", symbol);
+  }
+  $(".currency-js").html(symbol);
+}
+
 $(document).ready(function() {
+  if (sessionStorage && sessionStorage.getItem("currency")) {
+    $(".currency-js").html(sessionStorage.getItem("currency"));
+  } else {
+    $.getJSON("https://ipinfo.io/geo", function(data) {
+      var isEU = euCountries.indexOf(data.country)
+      setCurrency(isEU ? "€" : "$");
+    });
+  }
+
   $("#site-burger").click(function() {
     $("#site-burger").toggleClass("is-active");
     $(".navbar-menu").toggleClass("is-active");
