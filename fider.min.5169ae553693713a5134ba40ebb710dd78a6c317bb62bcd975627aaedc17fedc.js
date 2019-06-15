bdce48fc20ce4d3420c753a1dcb7fd273f162f43
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-06-15 15:00:54 +0000

    Deploy getfider/getfider.com to github.com/getfider/getfider.com.git:gh-pages

---
+++ fider.min.5169ae553693713a5134ba40ebb710dd78a6c317bb62bcd975627aaedc17fedc.js
@@ -0,0 +1,5 @@
+var euCountries=["AT","BE","BG","CY","CZ","DE","DK","EE","EL","ES","FI","FR","GB","HR","HU","IE","IT","LT","LU","LV","MT","NL","PL","PT","RO","SE","SI","SK"];function setCurrency(symbol){if(sessionStorage){sessionStorage.setItem("currency",symbol);}
+$(".currency-js").html(symbol);}
+$(document).ready(function(){if(sessionStorage&&sessionStorage.getItem("currency")){$(".currency-js").html(sessionStorage.getItem("currency"));}else{$.getJSON("https://api.ipdata.co?api-key=20338489079a61375c8d9cab64ac49dff68d5f2a8b71214c88944949",function(data){var isEU=euCountries.indexOf(data.country_code)
+setCurrency(isEU?"€":"$");},"jsonp");}
+$("#site-burger").click(function(){$("#site-burger").toggleClass("is-active");$(".navbar-menu").toggleClass("is-active");});$("#sidebar-burger").click(function(){$("#sidebar-burger").toggleClass("is-active");$(".sidebar").toggleClass("is-active");});});
